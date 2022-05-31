a25f4438fcfc5340d7ae08d21c5f5aa05621937f
Christian Schirmer <christian.schirmer@paufa.de>
2022-05-31 12:39:59 +0200

    Update

---
+++ DemoCompositeComponent/src/main/java/model/MessageTimeBean.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.time.LocalTime;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class MessageTimeBean {
+
+	public String getMessage() {
+		return "Kompositions Komponenten sind toll";
+	}
+	
+	
+	public LocalTime getTime() {
+		return LocalTime.now();
+	}
+}
